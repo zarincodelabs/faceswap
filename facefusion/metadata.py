712de816645dd01cdac9633368a704d58f8c8f9c
Zarin Code labs <host01@zarin.solutions>
2024-02-25 15:07:03 +0500

    relese

---
+++ facefusion/metadata.py
@@ -0,0 +1,13 @@
+METADATA =\
+{
+	'name': 'FaceFusion',
+	'description': 'Next generation face swapper and enhancer',
+	'version': '3.0',
+	'license': 'MIT',
+	'author': 'Anas Muhammad',
+	'url': 'https://anas.zarin.solutions'
+}
+
+
+def get(key : str) -> str:
+	return METADATA[key]
