b3e356707bb803150c633790eac263dcadd89ffa
Zarin Code labs <host01@zarin.solutions>
2024-02-25 21:23:33 +0500

    update2

--- facefusion/metadata.py
+++ facefusion/metadata.py
@@ -1,11 +1,11 @@
 METADATA =\
 {
-	'name': '<a href="https://anas.zarin.solutions" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="500" data-original-width="500" height="200" src="https://photos.zarin.solutions/logos/anas/web.png" width="200" /></a>',
+	'name': 'FaseSwap By Zarin Solutions',
 	'description': 'Next generation face swapper and enhancer',
 	'version': '3.0',
 	'license': 'MIT',
 	'author': 'Anas Muhammad',
-	'url': 'https://anas.zarin.solutions'
+	'url': 'https://zarin.solutions.com'
 }
 
 
